9173a6757c848d68187ec33db17e73e8ace23fe6
Aircongo <natelufuluabo@yahoo.ca>
2022-09-26 20:14:18 -0400

    made the navbar responsive to all screen sizes

--- free-quote/src/Components/Navbar Component/NavbarComponent.tsx
+++ free-quote/src/Components/Navbar Component/NavbarComponent.tsx
@@ -1,8 +1,9 @@
-import React from "react";
+import React, { useState } from "react";
 import './NavbarComponent.css';
 import { Link } from 'react-router-dom'
 
 const NavbarComponent = () => {
+    const [menuRequested, setMenuRequested] = useState(false);
     return (
         <header>
             <nav className='navbar'>
@@ -15,7 +16,21 @@ const NavbarComponent = () => {
                         <button className="navbar-applications-button">Applications</button>
                     </Link>
                 </section>
+                <button onClick={() => setMenuRequested(!menuRequested)} className="responsive-button">
+                <i className="fa fa-bars"></i>
+                </button>
             </nav>
+            {   menuRequested &&
+                <div className="responsive-menu-container">
+                    <Link to='/' className="responsive-link">
+                        Get a quote
+                    </Link>
+                    <hr></hr>
+                    <Link to='/applications' className="responsive-link">
+                        Applications
+                    </Link>
+                </div>
+            }
         </header>
     )
 }
